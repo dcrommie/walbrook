3fac20ddeb53cd5c9ade2a1d61657ecbb766c07d
MussadiqKhan <musadiq.khan@gmail.com>
2020-07-29 18:54:05 +0500

    Update wholesale.js

--- src/pages/wholesale.js
+++ src/pages/wholesale.js
@@ -21,7 +21,9 @@ const Wholesale = () => {
           contacttitle
           email
           contact
-          listitems
+          listitems {
+            title
+          }
           thumbnail
         }
       }
