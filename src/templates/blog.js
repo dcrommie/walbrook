602a0c28ddd41a1b91ab357de19ca2acd91a8aab
MussadiqKhan <musadiq.khan@gmail.com>
2020-08-04 02:44:08 +0500

    Update blog.js

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -29,25 +29,23 @@ const Blog = props => {
           <div className="blogpost-color">
             <div className="container">
               <h1>
-                {props.data.markdownRemark.frontmatter
+                {frontmatter
                   ? props.data.markdownRemark.frontmatter.blogtitle
                   : {}}
               </h1>
               <span>
-                {props.data.markdownRemark.frontmatter
+                {frontmatter
                   ? props.data.markdownRemark.frontmatter.author
                   : {}}
               </span>
               <span> &bull; </span>
               <span className="">
-                {props.data.markdownRemark.frontmatter
-                  ? props.data.markdownRemark.frontmatter.date
-                  : {}}
+                {frontmatter ? props.data.markdownRemark.frontmatter.date : {}}
               </span>
               <Image
                 alt="featured-image"
                 name={
-                  props.data.markdownRemark.frontmatter
+                  frontmatter
                     ? props.data.markdownRemark.frontmatter.thumbnail.replace(
                         "/images/",
                         ""
