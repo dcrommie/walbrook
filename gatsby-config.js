3ee9bc2c9e135608b900e3da6c8579efb64865e2
MussadiqKhan <musadiq.khan@gmail.com>
2020-08-04 23:47:01 +0500

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,21 +23,21 @@ module.exports = {
       },
     },
 
-    // {
-    //   resolve: "gatsby-transformer-remark",
-    //   options: {
-    //     plugins: [
-    //       "gatsby-remark-relative-images",
-    //       {
-    //         resolve: "gatsby-remark-images",
-    //         options: {
-    //           maxWidth: 750,
-    //           linkImagesToOriginal: false,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-netlify-cms`,
 
     `gatsby-transformer-sharp`,
