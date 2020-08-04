0101ceb067713275dfa3b8a2878231f966b0964c
MussadiqKhan <musadiq.khan@gmail.com>
2020-08-04 19:15:22 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,50 +1,5 @@
-// /**
-//  * Implement Gatsby's Node APIs in this file.
-//  *
-//  * See: https://www.gatsbyjs.org/docs/node-apis/
-//  */
-
 // // You can delete this file if you're not using it
 const path = require(`path`)
-
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-//   const { createPage } = actions
-
-//   const blogPostTemplate = path.resolve(`src/templates/blog.js`)
-
-//   const result = await graphql(`
-//     {
-//       allMarkdownRemark(
-//         sort: { order: DESC, fields: [frontmatter___date] }
-//         limit: 1000
-//       ) {
-//         edges {
-//           node {
-//             frontmatter {
-//               Slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-
-//   // Handle errors
-//   if (result.errors) {
-//     reporter.panicOnBuild(`Error while running GraphQL query.`)
-//     return
-//   }
-
-//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//     createPage({
-//       path: `${node.frontmatter.Slug}`,
-//       component: blogPostTemplate,
-//       context: {
-//         Slug: node.frontmatter.Slug,
-//       }, // additional data can be passed via context
-//     })
-//   })
-// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
