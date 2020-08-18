82d9c768279fee538a2c1913b8906f1d67555ba8
MussadiqKhan <musadiq.khan@gmail.com>
2020-08-19 03:34:10 +0500

    updates

--- src/components/newsletter-form.js
+++ src/components/newsletter-form.js
@@ -1,16 +1,61 @@
-import React from "react"
+import React, {useState} from "react"
+import addToMailchimp from 'gatsby-plugin-mailchimp'
 import "../styles/style.css"
 
 const NewsletterForm = () => {
+const [email, setEmail] = useState('')
+const [success, setSuccess] = useState(
+  ''
+)
+
+const onChange = (event) => {
+  setEmail(event.target.value);
+
+}
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addToMailchimp(email) // listFields are optional if you are only capturing the email address.
+    .then(data => {
+      // I recommend setting data to React state
+      // but you can do whatever you want (including ignoring this `then()` altogether)
+      console.log(data)
+      if(data.result === 'success'){
+        setSuccess('success')
+      } else if (data.result === 'error'){
+        setSuccess('error')
+
+      }
+    })
+    .catch(() => {
+      // unnecessary because Mailchimp only ever
+      // returns a 200 status code
+      // see below for how to handle errors
+    })
+  }
+
+  // 2. via `async/await`
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const result = await addToMailchimp(email)
+  //   // I recommend setting `result` to React state
+  //   // but you can do whatever you want
+  // }
+
   return (
     <div>
       <div className="newsletter">
+
+        {
+                  success === 'sucess' ?      <div>   <h3>Thank you for subscribing</h3></div> : success === 'error'? <div><h3>You are already subscribed</h3></div>:<div>
+
         <h3>Get the latest insights to your inbox</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="email-fields" style={{ marginBottom: "32px" }}>
             <input
               name="email"
-              className="email"
+              onChange={onChange}
+              value={email}              className="email"
               type="email"
               placeholder="Email"
             />
@@ -22,6 +67,8 @@ const NewsletterForm = () => {
             Subscribe
           </button>
         </form>
+        </div>
+}
       </div>
     </div>
   )
