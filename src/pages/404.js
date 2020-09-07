ffbad261e9db7bf110501b04134a1183317e834d
MussadiqKhan <musadiq.khan@gmail.com>
2020-09-07 18:36:52 +0500

    Updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,8 @@
 import React from "react"
-import SEO from "../components/seo"
+import { navigate } from "gatsby"
 
 const NotFoundPage = () => {
-  return (
-    <div>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  )
+  return navigate("/")
 }
 
 export default NotFoundPage
---
+++ src/pages/contact.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import Form from "../components/form"
+import SEO from "../components/seo"
+import Footer from "../components/footer"
+import Header from "../components/header"
+
+const Contact = () => {
+  return (
+    <div>
+      <SEO title="Contact Us" />
+
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        />
+      </Helmet>
+      <Header />
+      <div className="form" id="contact">
+        <div className="container">
+          <div className="main-form">
+            <div className="form-text">
+              <h2>Take care of your future today</h2>
+              <p style={{ margin: "0 auto", marginBottom: "32px" }}>
+                Get in touch so we can get you started
+              </p>
+            </div>
+            <Form />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Contact
