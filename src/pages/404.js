520ef9e8d3eb2da9b57e713702c54faf1a6b89d9
MussadiqKhan <musadiq.khan@gmail.com>
2020-09-07 19:07:59 +0500

    new changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 import "../styles/style.css"
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const Page404 = () => {
   return (
     <div className="main-404">
+      <Header />
       <div className="section">
         <h1 className="error">404</h1>
         <div className="page">
           Ooops!!! The page you are looking for is not found
         </div>
-        <Link className="back-home" to="/">
+        <Link className="main-button" to="/">
           Back to home
         </Link>
       </div>
+      <Footer />
     </div>
   )
 }
