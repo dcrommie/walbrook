5348799c3421d33216d91cc1f02e6940e7b9a5a2
MussadiqKhan <musadiq.khan@gmail.com>
2020-09-07 18:59:23 +0500

    404 updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from "react"
-import SEO from "../components/seo"
+import { Link } from "gatsby"
+import "../styles/style.css"
 
-const NotFoundPage = () => {
+const Page404 = () => {
   return (
-    <div>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="main-404">
+      <div className="section">
+        <h1 className="error">404</h1>
+        <div className="page">
+          Ooops!!! The page you are looking for is not found
+        </div>
+        <Link className="back-home" to="/">
+          Back to home
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default NotFoundPage
+export default Page404
