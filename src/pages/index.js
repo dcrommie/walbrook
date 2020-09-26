c331d249b088956c31b41857ead61dd9c8a8cb6b
MussadiqKhan <musadiq.khan@gmail.com>
2020-09-26 17:31:19 +0500

    new updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,8 +54,7 @@ const IndexPage = () => {
       </div>
       <Helmet>
         <link
-          rel="preload"
-          as="style"
+          rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous"
