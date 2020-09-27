767b532dcd4cfcf885cbfb289a2e00f9d54ebfd9
MussadiqKhan <musadiq.khan@gmail.com>
2020-09-28 00:32:33 +0500

    update for pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,11 +12,12 @@ const Contact = () => {
 
       <Helmet>
         <link
-          rel="preload"
-          as="style"
+          rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous"
+          media="print"
+          onload="this.media='all'"
         />
       </Helmet>
       <Header header="#contact" />
