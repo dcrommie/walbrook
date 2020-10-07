9d6476c50fa38e6521c8587d607ae3f22c96cf1b
MussadiqKhan <musadiq.khan@gmail.com>
2020-10-08 01:38:56 +0500

    new changes

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -33,8 +33,7 @@ const Privacy = () => {
 
       <Helmet>
         <link
-          rel="preload"
-          as="style"
+          rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous"
