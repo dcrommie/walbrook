e245b83cf1c0f3785c63c2f9ee4297b5cb39c02f
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2023-03-08 12:12:00 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint:
-          "https://smartphonestechie.us18.list-manage.com/subscribe/post?u=4c0afacac01cc8e432c16be94&amp;id=e6c69165df", // string; add your MC list endpoint here; see instructions below
+          "https://walbrook.us7.list-manage.com/subscribe/post?u=2dfc524c4881fb7f63ad5b3f5&amp;id=5a7bd20519&amp;f_id=00ee85e3f0", // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
